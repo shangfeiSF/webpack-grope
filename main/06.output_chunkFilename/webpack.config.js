4b6907f1d5d675c182c1155a063db33265a09dcb
shangfeiSF <tony20100125@126.com>
2017-03-15 17:44:42 +0800

    use chunks to split code and amd

---
+++ main/06.output_chunkFilename/webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path')
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+
+  entry: {
+    vendor: './vendor.js',
+    entry: './entry.js'
+  },
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.join(__dirname, 'build'),
+    chunkFilename: '[name].[id].[chunkhash].js',
+    publicPath: 'http://localhost:8080/assets/06/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'stage-1', 'stage-2', 'stage-3']
+            }
+          }
+        ],
+      }
+    ],
+  },
+}
