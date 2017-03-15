69aa59f6e186003aee2c130e363a0af4677b82cb
shangfeiSF <tony20100125@126.com>
2017-03-15 17:45:08 +0800

    chunk with user difened hash

---
+++ main/07.output_hash/src/entry.js
@@ -0,0 +1,30 @@
+import { TEN, TWENTY } from 'numbers'
+
+import * as math from 'math'
+
+let add = math.add(TEN, TWENTY)
+let subtract = math.subtract(TEN, TWENTY)
+let multiply = math.multiply(TEN, TWENTY)
+let divide = math.divide(TEN, TWENTY)
+
+console.log(add)
+console.log(subtract)
+console.log(multiply)
+console.log(divide)
+
+// require.ensure(dependencies: String[], callback: function(require), chunkName: String)
+require.ensure([], (require) => {
+  // loaded and executed `base.js`
+  let { info } = require('base').default
+  let { homeHandler, aboutHandler } = require('handlers').default
+  homeHandler(info)
+  aboutHandler(info)
+}, 'chunksInEntry_single')
+
+require.ensure(['base'], (require) => {
+  // loaded and but not executed `base.js`
+  let { home, about } = require('routes').default
+  let { homeHandler, aboutHandler } = require('handlers').default
+  homeHandler(home)
+  aboutHandler(about)
+}, 'chunksInEntry_double')
---
+++ main/07.output_hash/webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path')
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+
+  entry: {
+    vendor: './vendor.js',
+    entry: './entry.js'
+  },
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.join(__dirname, 'build'),
+    hashFunction: 'sha256',
+    hashDigest: 'base64',
+    hashDigestLength: 15,
+    chunkFilename: '[name].[id].[chunkhash].js',
+    publicPath: 'http://localhost:8080/assets/07/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'stage-1', 'stage-2', 'stage-3']
+            }
+          }
+        ],
+      }
+    ],
+  },
+}
