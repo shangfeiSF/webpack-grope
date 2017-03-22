1836d3ff84b19f58afc49eecf815c6779da58ab2
shangfeiSF <tony20100125@126.com>
2017-03-22 17:38:54 +0800

    bin/server and its gulpfile named 'lib/server.gulp.js' to realize auto-open

---
+++ bin/lib/server.gulp.js
@@ -0,0 +1,27 @@
+var fs = require('fs')
+var os = require('os')
+var path = require('path')
+
+var gulp = require('gulp')
+var open = require('gulp-open')
+var minimist = require('minimist')
+
+var options = minimist(process.argv.slice(2), {
+  string: 'urls',
+  default: {urls: ''}
+})
+
+var browser = os.platform() === 'linux' ?
+  'google-chrome' :
+  (os.platform() === 'darwin' ? 'google chrome' : (os.platform() === 'win32' ? 'chrome' : 'firefox'))
+
+gulp.task('open', function () {
+  var begin = gulp.src(__filename)
+
+  options.urls.forEach(function (url) {
+    begin.pipe(open({
+      uri: url,
+      app: browser
+    }))
+  })
+})
